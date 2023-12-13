e671f99f9a36f84abbc61e90108a3e44ad772ec4
Jay <jhajay438@gmail.com>
2023-12-13 11:50:51 +0530

    project upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,7 +71,7 @@ sr.reveal(".home-social-icon", { interval: 200 });
 /*SCROLL ABOUT*/
 sr.reveal(".about-img", {});
 sr.reveal(".about-subtitle", { delay: 400 });
-sr.reveal(".about-text", { delay: 400 });
+sr.reveal(".about-text", { delay: 100 });
 
 /*SCROLL SKILLS*/
 sr.reveal(".skills-subtitle", {});
@@ -80,7 +80,7 @@ sr.reveal(".skills-data", { interval: 100 });
 // sr.reveal(".skills-img", { delay: 600 });
 
 /*SCROLL projects*/
-sr.reveal(".project-img", { interval: 200 });
+sr.reveal(".project-img", { interval: 100 });
 
 /*SCROLL CONTACT*/
   // sr.reveal(".contact-input", { interval: 200 });
